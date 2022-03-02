39607e6d389a19b3a0332c40f5bae641def48ac3
MuhammadAshraf <ashrafhussain786@yahoo.com>
2022-03-02 11:18:32 +0500

    added translate.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Exchange from "./components/Exchange";
+import Translate from "./components/Translate";
 
 function App() {
   return (  
     <div className="App">      
-      <div><Exchange /></div>      
+      {/* <div><Exchange /></div>       */}
+      <div><Translate /></div>   
     </div>
   );
 }
